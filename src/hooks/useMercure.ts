54672686ea7dcf56d3775150b5eb3939fa5543a1
Omar Jbara <omar.jbara2@gmail.com>
2019-12-11 04:03:35 +0100

    - Fixed hook when using it with SSR: no need to execute the code in server-side

--- src/hooks/useMercure.ts
+++ src/hooks/useMercure.ts
@@ -2,6 +2,7 @@ import { DependencyList, useContext, useEffect, useMemo } from 'react'
 import { MercureContext } from '../providers/mercure'
 
 const useMercure = <D>(topic: string, onData: (data: D) => void, deps: DependencyList = []) => {
+  if (!(process as any).browser) return
   const { hubUrl, withCredentials } = useContext(MercureContext)
   const eventSource = useMemo(() => new EventSource(
       `${hubUrl}?topic=${encodeURIComponent(topic)}`,
